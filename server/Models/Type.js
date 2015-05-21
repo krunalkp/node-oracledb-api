3e55a5af0cab62a15f0ec593e928ae7c5eafec57
Marco Stagni <marcoponds@gmail.com>
2015-05-21 13:24:02 +0200

    working on models pt11

--- server/Models/Type.js
+++ server/Models/Type.js
@@ -21,11 +21,11 @@ var OracleDB = require("../DB").DB;
 var db = new OracleDB();
 
 db.connect().then(function(value) {
-    var query = "DROP TABLE vet_types PURGE;\n"
+    var query = "DROP TABLE vet_types PURGE;"
         query = "CREATE TABLE vet_types (";
         query += "type varchar2(20) NOT NULL, ";
         query += "CONSTRAINT vet_types_pk PRIMARY KEY (type));"
-        query += "COMMIT;\n";
+        query += "COMMIT;";
 
     console.log("about to create table vet_types");
     db.execute(query, function(result, err) {
