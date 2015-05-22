cd8d88e20fac82693b6198382a4549252697c1aa
Marco Stagni <marcoponds@gmail.com>
2015-05-22 11:10:37 +0200

    working on bug pt12

--- server/scripts/launch.sh
+++ server/scripts/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # moving to server folder
-cd Desktop/node-oracledb/node-oracledb-api/server
+cd ~/Desktop/node-oracledb/node-oracledb-api/server
 
 # pulling from remote
 cd .. && git pull
